7c94cab5f7d386683a5a62dc1c6650b22b94f854
Jimi Takamäki <jimi.takamaki@metropolia.fi>
2020-12-07 15:05:41 +0200

    RentoutusAktiviteetit.java-luokka lisätty ja kyselyä parannettu.

--- app/src/main/java/com/example/healthapp_v1/VastausActivity.java
+++ app/src/main/java/com/example/healthapp_v1/VastausActivity.java
@@ -9,7 +9,8 @@ import android.widget.TextView;
 
 public class VastausActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.project.MESSAGE";
-    private String vastaus;
+    private String vastausNimi;
+    private String vastausKuvaus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +18,19 @@ public class VastausActivity extends AppCompatActivity {
         setContentView(R.layout.activity_vastaus);
 
         Intent intent = getIntent();
-        vastaus = intent.getStringExtra(KyselyActivity.EXTRA_MESSAGE);
+        vastausNimi = intent.getStringExtra(KyselyActivity.EXTRA_MESSAGE);
+        vastausKuvaus = intent.getStringExtra(KyselyActivity.EXTRA_MESSAGE2);
 
-        TextView textView = findViewById(R.id.vastaus);
-        textView.setText(vastaus);
+        TextView textView1 = findViewById(R.id.vastausNimi);
+        TextView textView2 = findViewById(R.id.vastausKuvaus);
+        textView1.setText(vastausNimi);
+        textView2.setText(vastausKuvaus);
     }
 
     public void onButtonPress(View view) {
         if (view == findViewById(R.id.suorita)) {
             Intent intent = new Intent(this, MainActivity.class);
-            intent.putExtra(EXTRA_MESSAGE, vastaus);
+            intent.putExtra(EXTRA_MESSAGE, vastausNimi);
             startActivity(intent);
         } else if (view == findViewById(R.id.peruuta)) {
             Intent intent = new Intent(this, MainActivity.class);
